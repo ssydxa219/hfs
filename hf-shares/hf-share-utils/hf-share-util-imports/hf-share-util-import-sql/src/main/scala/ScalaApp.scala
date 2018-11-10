46e473e2316694ba9e18bf52631f320a2e74bf64
root <ssydxa219@126.com>
2018-11-10 12:50:09 +0800

    init big data architecture

---
+++ hf-shares/hf-share-utils/hf-share-util-imports/hf-share-util-import-sql/src/main/scala/ScalaApp.scala
@@ -0,0 +1,12 @@
+package com.jianbing.share.util.imports.sql
+
+object ScalaApp {
+
+  def main(args: Array[String]): Unit = {
+    println("Hello World")
+  }
+
+  def sayHello(x: String): Unit = {
+    println("hello," + x)
+  }
+}
