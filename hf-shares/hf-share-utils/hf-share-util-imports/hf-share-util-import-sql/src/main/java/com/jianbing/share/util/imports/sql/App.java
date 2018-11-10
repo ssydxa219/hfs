b4fc3509547603bd9cf30d67f59ef0144cc5816f
root <ssydxa219@126.com>
2018-11-10 12:51:36 +0800

    init big data architecture

---
+++ hf-shares/hf-share-utils/hf-share-util-imports/hf-share-util-import-sql/src/main/java/com/jianbing/share/util/imports/sql/App.java
@@ -0,0 +1,15 @@
+package com.jianbing.share.util.imports.sql;
+
+/**
+ * Hello world!
+ */
+public class App {
+
+    public static void main(String[] args) {
+       /* ScalaApp h;
+        h = new ScalaApp();
+        h.sayHello("scala");*/
+
+    }
+
+}
